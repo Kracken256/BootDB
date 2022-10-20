90d9826b3308efb2262f038898b8c1f3c72d11b1
Wesley Jones <wesjones2004@gmail.com>
2022-10-19 23:20:48 -0400

    Added comments. v1.1.0

--- BootDB.hpp
+++ BootDB.hpp
@@ -1,8 +1,8 @@
 /*
-This is a bootstraped database api for the protocal.
+This is a basic fast database header for the DecentMC protocol.
 In the future it my be replaced with a sqlite3 or whatever. I dont have time to learn a new API.
-Or another will be build on top of it. Maybe use vector of BOOTDB_BLOCK_SIZE to create a whole filesystem.
-
+Or another will be build on top of it. Maybe use vector of BOOTDB_BLOCK_SIZE to create a whole filesystem for variable length blocks. I dont foresee needing to
+store more then 512 bytes of the protocol per record.
 */
 
 #ifndef __DECENT_MC_BOOTDB__
@@ -20,6 +20,7 @@ Or another will be build on top of it. Maybe use vector of BOOTDB_BLOCK_SIZE to 
 // BOOTDB parameters
 #define BOOTDB_BLOCK_SIZE 512
 #define BOOTDB_HEADER_SIZE sizeof(BootDB::DatabaseHeader)
+
 namespace BootDB
 {
     uint64_t GetTime()
@@ -43,15 +44,14 @@ namespace BootDB
         const char magic_bytes[4] = {0x38, 0x53, 0x3f, 0x4f}; //  May change for production.
         const uint8_t version_id = 0x01;
         uint64_t time_created;
-        uint64_t time_updated;
         unsigned char owner_id[32];                  /// UUID of owning node
         unsigned char ed25519_signature_of_file[64]; // Uses ED25519 openssl implementation.
         unsigned char reserved[512];                 // May use later.
     };
-    enum RecordType // All records have a size of BOOTDB_BLOCK_SIZE variable.
+    enum RecordType // All records have a size of BOOTDB_BLOCK_SIZE variable. This is specific for the DecentMC protocol
     {
         MasterPrivateKey = 0, // Block 0
-        MasterPublicKey,      // Block 1
+        MasterPublicKey,
         IsVerifiedNode,
         NodeInfo,
         HostInfo,
@@ -64,10 +64,11 @@ namespace BootDB
         R4,
         R5,
         R6
-    }; // Undefined blocks will still be valid.
+    }; // Undefined RecordTypes will still be valid.
     class LocalDatabase
     {
     public:
+        // Singleton class
         static LocalDatabase *getInstance()
         {
             if (instance == 0)
@@ -76,6 +77,7 @@ namespace BootDB
             }
             return instance;
         }
+        // Init and write header
         static bool Init(std::string filepath)
         {
             FilePath = filepath;
@@ -122,6 +124,7 @@ namespace BootDB
         {
             return _database->is_open();
         }
+        // Get size in byts if database including header
         static long Size()
         {
             size_t size = 0;
@@ -132,6 +135,7 @@ namespace BootDB
             SetPos(tmp);
             return size;
         }
+        // Check if database is empty and has not been written to.
         static bool IsFresh()
         {
             if (Size() == BOOTDB_HEADER_SIZE)
@@ -140,10 +144,12 @@ namespace BootDB
             }
             return false;
         }
+        // Read record. Reads std::string of length == BOOTDB_BLOCK_SIZE
         static std::string ReadRecord(RecordType type)
         {
             return ReadRecord(type, BOOTDB_BLOCK_SIZE);
         }
+        // Reads record of specified length. prefered
         static std::string ReadRecord(RecordType type, int expected_length)
         {
             char buf[expected_length];
@@ -162,6 +168,7 @@ namespace BootDB
             std::string result = std::string(buf, expected_length);
             return result;
         }
+        // write record
         static bool WriteRecord(RecordType type, std::string data)
         {
             if (!IsOpen())
@@ -188,6 +195,7 @@ namespace BootDB
             Write(pos, block, BOOTDB_BLOCK_SIZE);
             return true;
         }
+        // Clear all data in database and reset size. Re-initilizes database header. (With new id)
         static void Clear()
         {
             _database->close();
@@ -197,6 +205,7 @@ namespace BootDB
             WriteDatabaseHeader();
             return;
         }
+        // Mind that this could cause seg faults
         ~LocalDatabase()
         {
             delete _database;
@@ -224,6 +233,7 @@ namespace BootDB
             }
             return tg; // Hope this doesn't come back to bite me.
         }
+        // Low level write
         static void Write(int pos, const char *buf, int size)
         {
             int tmp = GetPos();
@@ -232,6 +242,7 @@ namespace BootDB
             SetPos(tmp);
             return;
         }
+        // Check if database is valid. Based on length and magic bytes.
         static bool IsValid()
         {
             if (Size() < 4)
@@ -254,12 +265,12 @@ namespace BootDB
             }
             return true;
         }
+        // Writes database header with new random ID.
         static bool WriteDatabaseHeader()
         {
             struct DatabaseHeader dbh = DatabaseHeader();
             uint64_t time = GetTime();
             dbh.time_created = time;
-            dbh.time_updated = time;
             strcpy((char *)dbh.owner_id, create_rand_bytes(32).c_str());
             SetPos(0);
             Write(0, (const char *)&dbh, sizeof(dbh));
